cca23e57834ac9c53d49010ca60e6505584027cc
chou0728 <dean34520@gmail.com>
2019-02-14 00:51:33 +0800

    Modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,18 @@ set -e
 npm run build
 
 # cd 到打包後的dist目錄下 
-# cd dist
+cd dist
 
 # # 初始化git 並 add 與 commit
-# git init
-# git add -A
-# git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
-git add dist && git commit -m "Initial dist subtree commit"
-git subtree push --prefix dist origin gh-pages
+# git add dist && git commit -m "Initial dist subtree commit"
+# git subtree push --prefix dist origin gh-pages
 
 # 部署到 https://github.com/chou0728/flexible-form.git 分支為 gh-pages
-# git push -f https://github.com/chou0728/flexible-form.git master:gh-pages
+git push -f https://github.com/chou0728/flexible-form.git master:gh-pages 
+# 用vue cli官方的這個方式行不通，因為github規定github pages不能使用gh-pages(還是我先不要建立分支?)
 
 cd -
